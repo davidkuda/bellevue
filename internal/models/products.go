de120572691029047249e5222ee049b6cf8dfe15
David Kuda <davidkuda3@gmail.com>
2025-11-15 23:49:13 +0100

    get products into struct from db

---
+++ internal/models/products.go
@@ -0,0 +1,143 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+type Products []Product
+
+type Product struct {
+	ID            int
+	Name          string
+	Code          string
+	PricingMode   string
+	PriceCategory sql.NullString
+	Price         sql.NullInt64
+}
+
+type ProductFormConfig []ProductFormSpec
+
+type ProductFormSpec struct {
+	Code            string
+	HasCategories   bool
+	IsCustomAmount  bool
+	PriceCategories []string
+}
+
+type PriceKey struct {
+	Code     string // e.g. "lunch"
+	Category string // "regular", "" for custom
+}
+
+type ProductPrices map[PriceKey]int
+
+type ProductModel struct {
+	DB *sql.DB
+}
+
+func (m *ProductModel) GetProductFormConfig() (ProductFormConfig, error) {
+	stmt := `
+with product_form_specs as (
+       select p.code,
+              bool_or(p.price_category_id is not null) as has_categories,
+              bool_or(p.pricing_mode = 'custom') as is_custom_amount,
+              coalesce(
+                array_to_string(
+                  array_agg(distinct pc.name order by pc.name)
+                    filter (where pc.name is not null),
+                  ','
+                ),
+                ''
+              ) as price_categories
+         from bellevue.products p
+    left join bellevue.price_categories pc
+           on pc.id = p.price_category_id
+        where p.deleted_at is null
+        group by p.code
+)
+   select code, has_categories, is_custom_amount, price_categories
+     from product_form_specs
+left join product_form_order
+    using (code)
+ order by sort_order
+;
+	`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, fmt.Errorf("DB.Query(stmt): %v", err)
+	}
+	defer rows.Close()
+
+	var pfts ProductFormConfig
+	for rows.Next() {
+		var pft ProductFormSpec
+		var cats string
+		err = rows.Scan(
+			&pft.Code,
+			&pft.HasCategories,
+			&pft.IsCustomAmount,
+			&cats,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("for rows.Next(): %v", err)
+		}
+
+		if cats == "" {
+			pft.PriceCategories = nil
+		} else {
+			pft.PriceCategories = strings.Split(cats, ",")
+		}
+
+		pfts = append(pfts, pft)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows.Err(): %v", err)
+	}
+
+	return pfts, nil
+}
+
+func (m *ProductModel) GetAll() (Products, error) {
+	var err error
+
+	stmt := `
+	   SELECT p.id, p.name, p.code, p.pricing_mode, cat.name, p.price
+	     FROM products p
+	LEFT JOIN price_categories cat
+	       ON cat.id = p.price_category_id
+	;
+	`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, fmt.Errorf("DB.Query(stmt): %v", err)
+	}
+	defer rows.Close()
+
+	var products []Product
+	for rows.Next() {
+		var product Product
+		err = rows.Scan(
+			&product.ID,
+			&product.Name,
+			&product.Code,
+			&product.PricingMode,
+			&product.PriceCategory,
+			&product.Price,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("for rows.Next(): %v", err)
+		}
+		products = append(products, product)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows.Err(): %v", err)
+	}
+
+	return products, nil
+}
