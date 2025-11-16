2466a87b9cf4e2585f276c8c873832d0575d8917
David Kuda <davidkuda3@gmail.com>
2025-11-16 23:40:19 +0100

    add sql snippet to show consumptions

--- snippets/sql/queries.sql
+++ snippets/sql/queries.sql
@@ -9,3 +9,11 @@ from bellevue_origins b
 join auth.users u on u.id = b.user_id
 where email = 'test@kuda.ai';
 
+
+-- show consumptions:
+select date, u.first_name, p.name, pc.name as pricecat, c.unit_price, c.quantity, c.total_price
+from consumptions c
+join products p on product_id = p.id
+join auth.users u on c.user_id = u.id
+join price_categories pc on c.pricecat_id = pc.id;
+
