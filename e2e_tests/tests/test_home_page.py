dcc0860ce5f940c2239dc7845ff279ef093ca276
Edgar Haener <edgar.haner@gmail.com>
2025-12-22 09:16:30 +0000

    Adding a very threadbare test from loging to adding one activity

--- e2e_tests/tests/test_home_page.py
+++ e2e_tests/tests/test_home_page.py
@@ -1,4 +1,5 @@
 import re
+import time
 import random
 
 import pytest
@@ -6,6 +7,9 @@ from playwright.sync_api import Page, expect
 
 
 class TestSequencedSession:
+    """ One long test for now as the login we create gives us access to the app itself. 
+    TODO: add a test user and split out test of the activity from the test of the login page.
+    """
     def test_login_page(self, page: Page):
         page.goto("http://localhost:8875/")
 
@@ -35,3 +39,22 @@ class TestSequencedSession:
         page.fill('input[type="password"]', 'pa55word')
         page.click('#login-button')
         expect(page.get_by_role("button", name="Add your first activity")).to_be_visible()
+
+    def test_add_first_activity(self, page: Page):
+        """ Check we can add an activity """
+        page.get_by_role("button", name="Add your first activity").click()
+
+        # Find the container for breakfast and then the plus button within it
+        b_plus = page.locator(".fixed-price-activity", has=page.locator("#breakfast-quantity")).locator("button.plus")
+        b_plus.click()
+        b_plus.click()
+
+        # Submit activity
+        page.get_by_role("button", name="Add").click()
+
+        # Now should have table with three rows: header, breakfast we added and total
+        expect(page.locator("table tbody tr")).to_have_count(3)
+
+
+        breakfast_info = page.get_by_text("2 Breakfast for 9.00 CHF (regular)")
+        expect(breakfast_info).to_be_visible()
