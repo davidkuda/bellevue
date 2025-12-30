247b3ae3115fa1a91000aa7f64004c09119bd634
David Kuda <davidkuda3@gmail.com>
2025-12-30 23:04:12 +0100

    migrate snacks_chf to consumptions

--- migrations/archive/v1_to_v2.sql
+++ migrations/archive/v1_to_v2.sql
@@ -22,10 +22,11 @@ COMMIT;
 
 BEGIN;
 
--- TODO: snacks
 -- TODO: comments
 
 -----------------------------------------------------------------------
+-- bellevue_origin counts to consumptions:
+
 -- option 1: manual psql with variables
 
 -- \set product 'breakfast'
@@ -54,7 +55,6 @@ from	bellevue.bellevue_origins o
 where	o.breakfast_count > 0;
 
 
------------------------------------------------------------------------
 -- option 2: cross join
 
 -- v1:
@@ -179,3 +179,64 @@ GROUP BY
     unit_price;
 
 COMMIT;
+
+
+-----------------------------------------------------------------------
+-- bellevue_origin snacks_chf to consumptions:
+
+/*
+bellevue=# select * from bellevue_origins where snacks_chf > 0;
+
+ id  | user_id | activity_date | breakfast_count | lunch_count | dinner_count | coffee_count | sauna_count | lecture_count | snacks_chf |    comment    | total_price |          created_at
+-----+---------+---------------+-----------------+-------------+--------------+--------------+-------------+---------------+------------+---------------+-------------+-------------------------------
+ 100 |       1 | 2025-10-25    |               0 |           1 |            0 |            0 |           1 |             1 |        800 |               |        3850 | 2025-10-24 22:24:09.130195+00
+ 114 |      12 | 2025-10-16    |               1 |           1 |            1 |            3 |           1 |             1 |        900 |               |        6150 | 2025-11-12 15:11:54.645521+00
+ 125 |      13 | 2025-11-20    |               1 |           0 |            0 |            0 |           1 |             0 |        400 |               |        1950 | 2025-11-20 18:34:36.797032+00
+
+bellevue=# select * from consumptions limit 5;
+
+ id  | user_id | product_id | tax_id | pricecat_id | invoice_id |    date    | unit_price | quantity | total_price |          created_at
+-----+---------+------------+--------+-------------+------------+------------+------------+----------+-------------+-------------------------------
+ 168 |       1 |         11 |      1 |           3 |            | 2025-12-28 |       1100 |        1 |        1100 | 2025-12-28 23:16:51.567654+00
+ 169 |       1 |         12 |      1 |           3 |            | 2025-12-28 |       1500 |        3 |        4500 | 2025-12-28 23:16:51.567654+00
+ 170 |       1 |          7 |      1 |           2 |            | 2025-12-28 |       1300 |        2 |        2600 | 2025-12-28 23:16:51.567654+00
+ 171 |       1 |          4 |      1 |           1 |            | 2025-12-28 |        100 |        2 |         200 | 2025-12-28 23:16:51.567654+00
+ 172 |       1 |          9 |      1 |           2 |            | 2025-12-28 |        750 |        1 |         750 | 2025-12-28 23:16:51.567654+00
+(5 rows)
+
+bellevue=# select * from products;
+
+ id | financial_account_id | price_category_id | tax_id |     name      |   code    | pricing_mode | price |          valid_from           |          created_at           |          updated_at           | deleted_at
+----+----------------------+-------------------+--------+---------------+-----------+--------------+-------+-------------------------------+-------------------------------+-------------------------------+------------
+...
+ 15 |                    1 |                   |      1 | Snacks/Drinks | kiosk     | custom       |       | 2025-12-10 22:25:39.111992+00 | 2025-12-10 22:25:39.111992+00 | 2025-12-10 22:25:39.111992+00 |
+...
+*/
+
+begin;
+
+insert into bellevue.consumptions (
+    user_id,
+    product_id,
+    tax_id,
+    pricecat_id,
+    invoice_id,
+    date,
+    unit_price,
+    quantity,
+    created_at
+)
+select
+    user_id,
+    (select id from products where code = 'kiosk') as product_id,
+    (select id from taxes where code = 'B81') as tax_id,
+    null as pricecat_id,
+    null as invoice_id,
+    activity_date,
+    snacks_chf,
+    1 as quantity,
+    created_at
+from bellevue_origins
+where snacks_chf > 0;
+
+commit;
