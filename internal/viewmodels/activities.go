6a77488009f01f2f7681609c3d707b7ead90e3df
David Kuda <davidkuda3@gmail.com>
2026-04-03 21:42:21 +0200

    fetch a big representation of activity/consumptions

--- internal/viewmodels/activities.go
+++ internal/viewmodels/activities.go
@@ -1,6 +1,8 @@
 package viewmodels
 
 import (
+	"database/sql"
+	"fmt"
 	"time"
 
 	"github.com/davidkuda/bellevue/internal/models"
@@ -8,6 +10,7 @@ import (
 
 type ActivityViewModel struct {
 	DBModels models.Models
+	DB       *sql.DB
 }
 
 type Invoice struct {
@@ -19,16 +22,83 @@ type Activity struct {
 	Date         time.Time
 	Consumptions []Consumption
 	TotalPrice   int
+	Comment      string
 }
 
 type Consumption struct {
-	ProductName string
-	Amount      int
-	Count       int
-	UnitPrice   int
-	TotalPrice  int
+	ProductName  string
+	PriceCatName string
+	Amount       int
+	Count        int
+	UnitPrice    int
+	TotalPrice   int
 }
 
-func (m *ActivityViewModel) GetUninvoicedActivitiesForUser(userID int) (Invoice, error) {
+func (m *ActivityViewModel) GetUninvoicedActivitiesForUser(userID int) ([]Activity, error) {
+	type queryResult struct {
+		activityID  int
+		date        time.Time
+		name        string
+		pricecat    string
+		quantity    int
+		unit_price  int
+		total_price int
+	}
 
+	stmt := `
+	   SELECT a.id,
+	          a.date,
+	          p.name,
+	          pc.name,
+	          c.quantity,
+	          c.unit_price,
+	          c.total_price
+	     FROM consumptions c
+	LEFT JOIN activities a
+	       ON a.id = c.activity_id
+	LEFT JOIN products p
+	       ON p.id = c.product_id
+	LEFT JOIN price_categories pc
+	       ON pc.id = p.price_category_id
+	    WHERE a.invoice_id is null
+	      AND user_id = $1
+	 ORDER BY a.date DESC
+	;
+	`
+
+	// TODO: comments: maybe with map date=>comment
+
+	rows, err := m.DB.Query(stmt, userID)
+	if err != nil {
+		return nil, fmt.Errorf("DB.Query(stmt): %v", err)
+	}
+
+	defer rows.Close()
+
+	var res []queryResult
+
+	for rows.Next() {
+		var r queryResult
+		err = rows.Scan(
+			&r.activityID,
+			&r.date,
+			&r.name,
+			&r.pricecat,
+			&r.quantity,
+			&r.unit_price,
+			&r.total_price,
+		)
+
+		if err != nil {
+			return nil, fmt.Errorf("for rows.Next(): %v", err)
+		}
+
+		res = append(res, r)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("rows.Err(): %v", err)
+	}
+
+	return res, nil
 }
