a39845ac2d8a7e6b00aac382e68af2cba857cf44
David Kuda <davidkuda3@gmail.com>
2026-04-03 11:08:00 +0200

    make: use name migrate and remove db/ prefix

--- make/migrate.mk
+++ make/migrate.mk
@@ -1,40 +1,40 @@
 PG_DSN = postgres://davidkuda:@${DB_ADDRESS}/${DB_NAME}?sslmode=disable
 
-db/migrate/newsql:
+migrate/newsql:
 	@migrate create \
 	-seq \
 	-ext=.sql \
 	-dir=./migrations \
 	${name}
 
-db/migrate/up-all:
+migrate/up-all:
 	@migrate \
 	-path=./migrations \
 	-database=${PG_DSN} \
 	up
 
-db/migrate/version:
+migrate/version:
 	migrate \
 	-path=./migrations/ \
 	-database=${PG_DSN} \
 	version
 
 # force V: Set version V but don't run migration (ignores dirty state)
-db/migrate/force:
+migrate/force:
 	@migrate \
 	-path=./migrations/ \
 	-database=${PG_DSN} \
 	force ${version}
 
 # migrate down one step
-db/migrate/down-1:
+migrate/down-1:
 	@migrate \
 	-path=./migrations/ \
 	-database=${PG_DSN} \
 	down 1
 
 # migrate up one step
-db/migrate/up-1:
+migrate/up-1:
 	@migrate \
 	-verbose \
 	-path=./migrations/ \
