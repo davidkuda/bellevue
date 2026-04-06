99a19ccee790b7779aa336d6569fe3885c3d97bb
David Kuda <davidkuda3@gmail.com>
2026-04-06 17:02:43 +0200

    include donations as financial account

--- migrations/000006_update-financial-accounts.down.sql
+++ migrations/000006_update-financial-accounts.down.sql
@@ -12,4 +12,11 @@ where id = 3;
 -- update products ID not reversed with intent.
 -- this was just a misstake.
 
+delete from financial_accounts
+where code = 3410;
+
+update products
+set financial_account_id = 1
+where code = 'donations';
+
 commit;
--- migrations/000006_update-financial-accounts.up.sql
+++ migrations/000006_update-financial-accounts.up.sql
@@ -12,4 +12,24 @@ update financial_accounts
 set view_name = 'Kiosk'
 where id = 3;
 
+insert into financial_accounts (
+	tax_id,
+	code,
+	name,
+	view_name
+) values (
+	(select id from taxes where mwst_satz = 0),
+	3410,
+	'Spenden - Diverse',
+	'Spenden'
+);
+
+update products
+set financial_account_id = (
+	select id
+	from financial_accounts
+	where code = 3410
+)
+where code = 'donations';
+
 commit;
